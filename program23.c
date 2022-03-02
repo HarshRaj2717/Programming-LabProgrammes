10011ef0b886d1200de8dfd59dd5b3a908b5d2c4
Harsh Raj <harshraj2717@gmail.com>
2022-03-03 02:25:09 +0530

    revert program23.c

--- program23.c
+++ program23.c
@@ -1,25 +1,47 @@
 /*Write a program to convert decimal number in to binary number.*/
 
 #include <stdio.h>
+#include <math.h>
 
-void main(void) 
-{
-    int reverseBin[10], number;
-    int i=9;
-    printf("Enter a decimal number : ");
-    scanf("%d", &number);
+void main(){
+    float num, decimalPart, binDec=0, binaryConversion;
+    int integerPart,a,binInt=0,p1=0, p2=-1;
     
-    while(number)
+    printf("Enter a number : ");
+    scanf("%f",&num);
+    
+    integerPart=num;
+    decimalPart=num-integerPart;
+    
+    // binary conversion of the integer part
+    while (integerPart !=0)
     {
-        reverseBin[i] = number % 2;
-        number = number / 2;
-        i--;
+        a=integerPart%2;
+        binInt=binInt+pow(10,p1)*a;
+        integerPart/=2;
+        p1++;
     }
 
-    printf("The binary conversion of given number is : ");
-
-    for(int j=i+1; j<10; j++)
+    // binary conversion of the decimal part
+    while (p2> -4)
     {
-      printf("%d", reverseBin[j]);
+      decimalPart=decimalPart*2;
+      if (decimalPart<1)
+      {
+        binDec=binDec+pow(10,p2)*0;        
+      }
+      else if (decimalPart>=1){
+        binDec=binDec+pow(10,p2)*1;
+        decimalPart-=1;
+      }
+      p2--;
     }
+    // overall binary conversion
+    binaryConversion=binInt+binDec;    
+    
+
+
+
+    printf("The Binary conversion of the number is %.3f\n", binaryConversion);
+
 }
